46eab9b6daeb9d63e1a350abc20b267d89e135a8
wowilliam <wo.william@hotmail.com>
2022-10-05 21:15:18 -0300

    Fundamentos Java

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 10;
 
         if(nota >= 7 )
         System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7) // true ou false
+        System.out.println("Prova Recuperação");
         
         
         else 
